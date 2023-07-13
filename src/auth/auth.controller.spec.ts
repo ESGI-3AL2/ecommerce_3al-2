6d69aba811c23ffceff2ea51a05a98ab4c724d09
LoicZHU <zhu.loic@gmail.com>
2023-07-13 19:14:18 +0200

    test: quick fix AuthController

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,52 +1,68 @@
-import { TestingModule, Test } from "@nestjs/testing";
-import { User } from "../user/schemas/user.schema";
-import { UserService } from "../user/user.service";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-import { Role } from "./enums/role.enum";
-import { JwtAuthGuard } from "./guards/jwt.guard";
-import { RolesGuard } from "./guards/roles.guard";
+import { JwtService } from '@nestjs/jwt';
+import { TestingModule, Test } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+
+import { User } from '../user/schemas/user.schema';
+import { UserService } from '../user/user.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Role } from './enums/role.enum';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 describe('AuthController', () => {
-    let authController: AuthController;
-    let authService: AuthService;
-    let userService: UserService;
-  
-    beforeEach(async () => {
-      const module: TestingModule = await Test.createTestingModule({
-        controllers: [AuthController],
-        providers: [AuthService, UserService, JwtAuthGuard, RolesGuard],
-      }).compile();
-  
-      authController = module.get<AuthController>(AuthController);
-      authService = module.get<AuthService>(AuthService);
-      userService = module.get<UserService>(UserService);
-    });
-  
-    it('should be defined', () => {
-      expect(authController).toBeDefined();
-    });
-  
-    describe('/register', () => {
-        it('should register a new user', async () => {
-            const userDto = { 
-                username: 'test', 
-                email: 'test@test.com', 
-                password: 'test', 
-                roles: [Role.User]
-            };
+  let authController: AuthController;
+  let authService: AuthService;
+  let userService: UserService;
 
-            const createdUser: User = {
-                username: userDto.username,
-                email: userDto.email,
-                password: userDto.password,
-                roles: userDto.roles,
-            };
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        UserService,
+        JwtAuthGuard,
+        RolesGuard,
+        {
+          provide: JwtService,
+          useValue: {}, // You can provide a mock or an actual instance of JwtService here
+        },
+        {
+          provide: getModelToken('User'),
+          useValue: {}, // You can provide a mock or an actual instance of UserModel here
+        },
+      ],
+    }).compile();
 
-            jest.spyOn(userService, 'addUser').mockImplementation(() => Promise.resolve(createdUser));
-            expect(await authController.register(userDto)).toBe(createdUser);
-        });
-   
-      });
-  
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
   });
+
+  it('should be defined', () => {
+    expect(authController).toBeDefined();
+  });
+
+  describe('/register', () => {
+    it('should register a new user', async () => {
+      const userDto = {
+        username: 'test',
+        email: 'test@test.com',
+        password: 'test',
+        roles: [Role.User],
+      };
+
+      const createdUser: User = {
+        username: userDto.username,
+        email: userDto.email,
+        password: userDto.password,
+        roles: userDto.roles,
+      };
+
+      jest
+        .spyOn(userService, 'addUser')
+        .mockImplementation(() => Promise.resolve(createdUser));
+      expect(await authController.register(userDto)).toBe(createdUser);
+    });
+  });
+});
