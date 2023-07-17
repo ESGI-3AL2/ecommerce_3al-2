435d1cd040044c26fb64fd9fe58764e059c8e0b4
James <james.bidule@gmail.com>
2023-07-17 15:01:26 +0200

    fix(auth): implemented new auth tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -9,41 +9,72 @@ import { AuthService } from './auth.service';
 import { Role } from './enums/role.enum';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { RolesGuard } from './guards/roles.guard';
+import { UnauthorizedException } from '@nestjs/common';
+
 
 describe('AuthController', () => {
   let authController: AuthController;
   let authService: AuthService;
   let userService: UserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [
-        AuthService,
-        UserService,
-        JwtAuthGuard,
-        RolesGuard,
-        {
-          provide: JwtService,
-          useValue: {}, // You can provide a mock or an actual instance of JwtService here
-        },
-        {
-          provide: getModelToken('User'),
-          useValue: {}, // You can provide a mock or an actual instance of UserModel here
-        },
-      ],
-    }).compile();
+  describe('when user is unauthenticated', () => {
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [AuthController],
+        providers: [
+          AuthService,
+          { 
+            provide: UserService, 
+            useValue: {
+              addUser: jest.fn().mockImplementation(() => {
+                throw new UnauthorizedException();
+              }),
+            },
+          },
+          { provide: JwtService, useValue: {} },
+          { provide: getModelToken('User'), useValue: {} },
+        ],
+      }).compile();
 
-    authController = module.get<AuthController>(AuthController);
-    authService = module.get<AuthService>(AuthService);
-    userService = module.get<UserService>(UserService);
+      authController = module.get<AuthController>(AuthController);
+      authService = module.get<AuthService>(AuthService);
+      userService = module.get<UserService>(UserService);
+    });
+
+    it('should throw UnauthorizedException', async () => {
+      const userDto = {
+        username: 'test',
+        email: 'test@test.com',
+        password: 'test',
+        roles: [Role.User],
+      };
+      await expect(authController.register(userDto)).rejects.toThrow(UnauthorizedException);
+    });
   });
+  describe('when user is authenticated', () => {
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [AuthController],
+        providers: [
+          AuthService,
+          { 
+            provide: UserService, 
+            useValue: {
+              addUser: jest.fn().mockImplementation((user) => Promise.resolve(user)),
+            },
+          },
+          JwtAuthGuard,
+          RolesGuard,
+          { provide: JwtService, useValue: {} },
+          { provide: getModelToken('User'), useValue: {} },
+        ],
+      }).compile();
 
-  it('should be defined', () => {
-    expect(authController).toBeDefined();
-  });
-
-  describe('/register', () => {
+      authController = module.get<AuthController>(AuthController);
+      authService = module.get<AuthService>(AuthService);
+      userService = module.get<UserService>(UserService);
+    });
+  
     it('should register a new user', async () => {
       const userDto = {
         username: 'test',
@@ -51,18 +82,20 @@ describe('AuthController', () => {
         password: 'test',
         roles: [Role.User],
       };
-
+  
       const createdUser: User = {
         username: userDto.username,
         email: userDto.email,
         password: userDto.password,
         roles: userDto.roles,
       };
-
-      jest
-        .spyOn(userService, 'addUser')
-        .mockImplementation(() => Promise.resolve(createdUser));
-      expect(await authController.register(userDto)).toBe(createdUser);
+  
+      (userService.addUser as jest.Mock).mockResolvedValue(createdUser);
+      const result = await authController.register(userDto);
+      expect(result.username).toBe(createdUser.username);
+      expect(result.email).toBe(createdUser.email);
+      expect(result.password).toBe(createdUser.password);
+      expect(result.roles).toEqual(createdUser.roles);
     });
   });
 });
